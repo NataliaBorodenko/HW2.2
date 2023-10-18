ac00a5efc995281e2571c15e4542e8702f42a941
Natalia Borodenko <borodenkonatalia9@gmail.com>
2023-10-18 23:25:30 +0300

    New2

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,15 @@ public class Main {
         int replenishment = 1100;
 
         int bonus;
-            if (replenishment > 1000){
-                bonus = replenishment / 100;
-            }else {
-                bonus = 0;
-            }
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
         int result = initially + replenishment + bonus;
 
-            System.out.println(" Сумма бонусов:" + bonus);
-            System.out.println (" Итоговый счет:" + result);
-
-
+        System.out.println(" Сумма бонусов:" + bonus);
+        System.out.println(" Итоговый счет:" + result);
 
     }
 }
